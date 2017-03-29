8384cc9052f8580a3c4eb9bad12123cd9ced4d97
lucian <lucian.dizzy@gmail.com>
2017-03-29 19:01:26 +0200

    added few models and initial configuration

--- sisweb/sisweb/settings.py
+++ sisweb/sisweb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mygames'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'sisweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'temaplates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
